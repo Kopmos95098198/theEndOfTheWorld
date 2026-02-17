30b6697e53305cf196b9ee93ceacde250cb7c4c6
Kopmos95098198 <69678071+Kopmos95098198@users.noreply.github.com>
2026-02-17 20:03:54 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Good bye World!" << endl;
-    cout << 666 * 444;
+    string letters[5] = 
+        {"a", "e", "i", "o", "u"};
+    
+    string enterSentence;
+    getline(cin, enterSentence);
+
+    int getArraySize = sizeof(letters) / sizeof(letters[0]);
+    cout << getArraySize;
+    
+        
+
+    
+
     return 0;
 
 }
