e854ef164f1126f2a4391a0edc68a7f87d738d6c
Kopmos95098198 <69678071+Kopmos95098198@users.noreply.github.com>
2026-02-19 15:33:32 +0800

    1

--- main.cpp
+++ main.cpp
@@ -2,29 +2,12 @@
 using namespace std;
 
 int main() {
-    int x, y, z;
+   
+    int x;
 
-    cin >> x >> y >> z;
+    cin >> x;
     
-    if (x == y && y == z) {
 
-        cout << 10000 + x * 1000;
-
-    } else if (x == y) {
-
-        cout << 1000 + x * 100;
-
-    } else if (y == z) {
-
-        cout << 1000 + y * 100; 
-
-    } else if (x == z) {
-
-        cout << 1000 + z * 100;
-
-    } else {
-        cout << max({ x, y, z}) * 100;
-    }
 
     
     return 0; 
